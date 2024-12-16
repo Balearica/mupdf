a4e3e9e89c2ea49f8463c1f4ac7aab1cec151288
Balearica <admin@scribeocr.com>
2024-12-15 22:25:14 -0800

    Created docker build script for better portability; updated so build runs on Windows Node.js

---
+++ build-with-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+EM_VERSION=3.1.38
+
+# docker pull emscripten/emsdk:$EM_VERSION
+docker run \
+  -v $PWD:/src \
+  -v $PWD/build/wasm/cache:/emsdk/upstream/emscripten/cache \
+  emscripten/emsdk:$EM_VERSION \
+  sh -c 'bash ./build.sh'
